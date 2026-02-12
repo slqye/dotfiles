2a364c0ae203f96f9a1eca04e4c7873264fcd03b
Slqye <69047488+slqye@users.noreply.github.com>
2026-02-12 12:39:25 +0100

    feat: working pylsp

--- nvim/lsp/pylsp.lua
+++ nvim/lsp/pylsp.lua
@@ -1,1 +1,29 @@
-return {}
+return {
+	settings = {
+		pylsp = {
+			plugins = {
+				pylint = {
+					enabled = true,
+					executable = "pylint",
+					args = {
+						"--disable=C0114",
+						"--disable=C0116",
+						"--disable=W0311",
+					},
+				},
+				autopep8 = {
+					enabled = false,
+				},
+				pyflakes = {
+					enabled = false,
+				},
+				pycodestyle = {
+					enabled = false,
+				},
+				mccabe = {
+					enabled = false,
+				},
+			},
+		},
+	},
+}
--- nvim/lua/config/others.lua
+++ nvim/lua/config/others.lua
@@ -1,1 +1,2 @@
 vim.diagnostic.config({ virtual_text = true })
+vim.g.python_recommended_style = 0
