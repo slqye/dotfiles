aecdc06e5cf4ace84bb86f3aa37e4e9b18111ab6
Slqye <69047488+slqye@users.noreply.github.com>
2026-02-17 14:27:03 +0100

    feat: themes sctructure and TokyoNight theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,11 +7,9 @@ require("config.others")
 -- plugins
 require("plugins.dependencies")
 require("plugins.lsp")
+require("plugins.themes")
 require("plugins.gitsigns")
 require("plugins.oil")
 require("plugins.telescope")
 require("plugins.markview")
 require("plugins.smear_cursor")
-
--- themes
-vim.cmd("colorscheme catppuccin")
---
+++ nvim/lua/plugins/themes.lua
@@ -0,0 +1,8 @@
+vim.pack.add({
+	{
+		name="tokyonight",
+		src="https://github.com/folke/tokyonight.nvim"
+	}
+})
+
+vim.cmd("colorscheme tokyonight")
